bb492a2082d316ba686d6d550920c01c6a81f2f5
ganshunwei <ganshunwei@gank.local>
2016-06-20 14:13:10 +0800

    add request

---
+++ MSBase/Class/Unit/Http/MSRequest.h
@@ -0,0 +1,13 @@
+//
+//  MSRequest.h
+//  MSBase
+//
+//  Created by ganshunwei on 16/6/1.
+//  Copyright © 2016年 ganshunwei. All rights reserved.
+//
+
+#import "YTKRequest.h"
+
+@interface MSRequest : YTKRequest
+
+@end
---
+++ MSBase/Class/Unit/Http/MSRequest.m
@@ -0,0 +1,13 @@
+//
+//  MSRequest.m
+//  MSBase
+//
+//  Created by ganshunwei on 16/6/1.
+//  Copyright © 2016年 ganshunwei. All rights reserved.
+//
+
+#import "MSRequest.h"
+
+@implementation MSRequest
+
+@end
