bac2911266260a224a2df31569445a546db81be4
ganshunwei <ganshunwei@gank.local>
2017-02-23 13:56:47 +0800

    merg

--- MSBase/Class/Constant/MSDefine.h
+++ MSBase/Class/Constant/MSDefine.h
@@ -9,7 +9,17 @@
 #ifndef MSDefine_h
 #define MSDefine_h
 
-//um 58ad5e5899f0c73323001a2b
+/** 简写 */
+#define SBApplication        [UIApplication sharedApplication]
+#define SBKeyWindow          [UIApplication sharedApplication].keyWindow
+#define SBAppDelegate        [UIApplication sharedApplication].delegate
+#define SBUserDefaults      [NSUserDefaults standardUserDefaults]
+#define SBNotificationCenter [NSNotificationCenter defaultCenter]
+#define SBViewWithTag(_OBJECT, _TAG)    [_OBJECT viewWithTag : _TAG]
+#define SBAppDisplayName [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleDisplayName"]
+
+#define ELLandscape (SCREEN_WIDTH > SCREEN_HEIGHT)
+#define EL3rdConfig(key) [[[[NSBundle mainBundle] infoDictionary] objectForKey:@"EL3rdConfig"] objectForKey:key]
 #define kThemeColor        RGB_A(247, 74,112, 1.0)//主题色
 #define kShowWelcomePage              @"kShowWelcomePage"           //显示引导页
 
